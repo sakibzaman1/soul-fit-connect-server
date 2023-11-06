337052d5eae0da8a39e7be79fe68d7f91f943d8a
sakibzaman1 <sakibzaman20@gmail.com>
2023-11-06 21:49:23 +0600

    my schedules post method has been implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikmm0oq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,36 +28,57 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const serviceCollection = client.db('soulFitServiceDB').collection('services');
+    const serviceCollection = client
+      .db("soulFitServiceDB")
+      .collection("services");
+    const mySchedulesCollection = client
+      .db("soulFitServiceDB")
+      .collection("myschedules");
 
-     // Read data
+    // Read data
     // Read services
-    app.get('/services', async(req, res)=> {
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    app.get('/services/:id', async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await serviceCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
       res.send(result);
-  })
+    });
 
+    // Read my schedules
+    app.get("/myschedules", async (req, res) => {
+      const cursor = mySchedulesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     // Create data
+    // Create data
     // post service
-    app.post('/services', async(req, res)=> {
-        const newService = req.body;
-        console.log(newService)
-        const result = await serviceCollection.insertOne(newService);
-        res.send(result);
+    app.post("/services", async (req, res) => {
+      const newService = req.body;
+      console.log(newService);
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // post my schedules
+    app.post("/myschedules", async (req, res) => {
+      const myNewService = req.body;
+      console.log(myNewService);
+      const result = await mySchedulesCollection.insertOne(myNewService);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -65,11 +86,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Soul Server is running");
+});
 
-app.get('/', (req, res)=> {
-    res.send('Soul Server is running')
-})
-
-app.listen(port, ()=> {
-    console.log(`Soul server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Soul server is running on port ${port}`);
+});
